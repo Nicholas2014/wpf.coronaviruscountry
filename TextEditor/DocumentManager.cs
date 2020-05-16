62f6f169356e2151b0b2336204d9484e3cfbb109
citywalker <mxq008@126.com>
2020-05-16 22:01:33 +0800

    chaper 8 finished

---
+++ TextEditor/DocumentManager.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace TextEditor
+{
+    public class DocumentManager
+    {
+        private string _currentFile;
+        private readonly RichTextBox text;
+
+        public DocumentManager(RichTextBox text)
+        {
+            this.text = text;
+        }
+
+        public bool OpenDocument()
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                _currentFile = ofd.FileName;
+                using (Stream s = ofd.OpenFile())
+                {
+                    var range = new TextRange(text.Document.ContentStart, text.Document.ContentEnd);
+                    range.Load(s, DataFormats.Rtf);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool SaveDocument()
+        {
+            if (string.IsNullOrEmpty(_currentFile))
+            {
+                return SaveDocumentAs();
+            }
+            else
+            {
+                using (Stream s = new FileStream(_currentFile, FileMode.Create))
+                {
+                    var range = new TextRange(text.Document.ContentStart, text.Document.ContentEnd);
+                    range.Save(s, DataFormats.Rtf);
+                }
+
+                return true;
+            }
+        }
+
+        public bool SaveDocumentAs()
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == true)
+            {
+                _currentFile = sfd.FileName;
+
+                return SaveDocument();
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ TextEditor/TextEditorToolBar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// TextEditorToolBar.xaml 的交互逻辑
+    /// </summary>
+    public partial class TextEditorToolBar : UserControl
+    {
+        public TextEditorToolBar()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (double i = 8; i < 48; i+=2)
+            {
+                fontSize.Items.Add(i);
+            }
+        }
+    }
+}
