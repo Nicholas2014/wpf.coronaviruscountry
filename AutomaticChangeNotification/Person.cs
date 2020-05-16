31225b891358bb5d63652656b97d63acdea166ca
citywalker <mxq008@126.com>
2020-05-16 09:21:33 +0800

    chapter 6 databing finished

---
+++ AutomaticChangeNotification/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticChangeNotification
+{
+    class Person
+    {
+        public event EventHandler FirstNameChanged;
+        private void OnFirstNameChanged()
+        {
+            FirstNameChanged?.Invoke(this, EventArgs.Empty);
+        }
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnFirstNameChanged();
+            }
+        }
+
+    }
+}
