8add30f48c0b606a81d4ff6bdf461d18a9a65cb0
citywalker <mxq008@126.com>
2020-05-21 14:15:28 +0800

    Resources And Styles Finished

---
+++ ContactManager/Model/States.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Model
+{
+    public static class States
+    {
+        private static readonly List<string> _names;
+
+        static States()
+        {
+            _names = new List<string>(50);
+            _names.Add("阿联酋");
+            _names.Add("阿富汗");
+            _names.Add("安提瓜和巴布达");
+            _names.Add("安圭拉");
+            _names.Add("阿尔巴尼亚");
+            _names.Add("亚美尼亚");
+            _names.Add("安哥拉");
+            _names.Add("南极洲");
+            _names.Add("阿根廷");
+            _names.Add("美属萨摩亚");
+            _names.Add("奥地利");
+            _names.Add("澳大利亚");
+            _names.Add("阿鲁巴");
+            _names.Add("奥兰群岛");
+            _names.Add("阿塞拜疆");
+            _names.Add("波黑");
+            _names.Add("巴巴多斯");
+            _names.Add("孟加拉");
+            _names.Add("比利时");
+            _names.Add("布基纳法索");
+            _names.Add("保加利亚");
+            _names.Add("巴林");
+            _names.Add("布隆迪");
+            _names.Add("贝宁");
+            _names.Add("圣巴泰勒米岛");
+            _names.Add("百慕大");
+            _names.Add("文莱");
+            _names.Add("玻利维亚");
+            _names.Add("荷兰加勒比区");
+            _names.Add("巴西");
+        }
+
+        public static IList<string> GetNames()
+        {
+            return _names;
+        }
+    }
+
+}
---
+++ ContactManager/Shell.xaml.cs
@@ -0,0 +1,71 @@
+﻿using ContactManager.Presenters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactManager
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+            DataContext = new ApplicationPresenter(this, new Model.ContactRepository());
+        }
+
+        public void AddTab<T>(PresenterBase<T> presenter)
+        {
+            TabItem newTab = null;
+            for (int i = 0; i < tabs.Items.Count; i++)
+            {
+                var existTab = tabs.Items[i] as TabItem;
+                if (existTab.DataContext.Equals(presenter))
+                {
+                    tabs.Items.Remove(existTab);
+                    newTab = existTab;
+                    break;
+                }
+            }
+
+            if (newTab == null)
+            {
+                newTab = new TabItem();
+                var headerBinding = new Binding(presenter.TabHeaderPath);
+                BindingOperations.SetBinding(newTab, TabItem.HeaderProperty, headerBinding);
+                newTab.DataContext = presenter;
+                newTab.Content = presenter.View;
+            }
+
+            tabs.Items.Insert(0, newTab);
+            newTab.Focus();
+        }
+
+        public void RemoveTab<T>(PresenterBase<T> presenter)
+        {
+            for (int i = 0; i < tabs.Items.Count; i++)
+            {
+                var existTab = tabs.Items[i] as TabItem;
+                if (existTab.DataContext.Equals(presenter))
+                {
+                    tabs.Items.Remove(existTab);
+                    break;
+                }
+            }
+        }
+    }
+
+}
